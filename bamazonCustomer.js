df6f69ab2867dec06faa590ac206dee7246e8447
rmoj <rmoj99@gmail.com>
2018-08-27 15:42:19 -0400

    Create: getOrder(), rename getCustomerInput to isBuying()

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 require('console.table');
 
 var connection = mysql.createConnection({
@@ -11,24 +12,84 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
-  showProducts();
-  connection.end();
+  console.log('connected to MySQL as id ' + connection.threadId);
+  isBuying();
+  // connection.end();
 });
 
-function showProducts() {
-  connection.query('SELECT * FROM products WHERE stock_quantity > 0', function(
-    err,
-    res
-  ) {
-    if (err) throw err;
-    console.log(
-      '\n----------------------------------------------------------- B A M A Z O N -----------------------------------------------------'
-    );
-    console.log(
-      '-------------------------------------------------------------------------------------------------------------------------------\n'
-    );
+function showProducts(func) {
+  var query = connection.query(
+    'SELECT item_id, product_name, price, stock_quantity FROM products WHERE stock_quantity > 0',
+    function(err, res) {
+      if (err) throw err;
+      console.log(
+        '\n----------------------------------------------------------- B A M A Z O N -----------------------------------------------------'
+      );
+      console.log(
+        '-------------------------------------------------------------------------------------------------------------------------------\n'
+      );
 
-    console.table(res);
-  });
+      console.table(res);
+      console.log(res[0].price);
+      console.log(query._results[0][0].price);
+      func();
+    }
+  );
+}
+
+function isBuying() {
+  inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'wantsToBuy',
+        message: 'Do you want to buy something?'
+      }
+    ])
+    .then(function(user) {
+      if (user.wantsToBuy == true) {
+        showProducts(getOrder);
+      } else {
+        connection.end();
+        console.log('connection ended');
+      }
+    });
+}
+
+function getOrder() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'itemId',
+        message: 'Enter item id of product: '
+      },
+      {
+        type: 'input',
+        name: 'quantity',
+        message: 'Enter quantity to buy: '
+      }
+    ])
+    .then(function(product) {
+      console.log(product.itemId, product.quantity);
+      connection.end();
+    });
+}
+
+function updateProducts(item, qty, func) {
+  connection.query(
+    'UPDATE products SET ? WHERE ?',
+    [
+      {
+        stock_quantity: qty
+      },
+      {
+        item_id: item
+      }
+    ],
+    function(err, res) {
+      console.log(res.affectedRows + ' products updated!\n');
+      // Call deleteProduct AFTER the UPDATE completes
+    }
+  );
 }
